aaefd7e36659191ec2a8781f07bc89b085828296
avenirs05 <avenirs05@gmail.com>
2017-11-17 01:56:32 +0200

    now if you hover on menu-item it is yellow color

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
 $(function () {    
 
     // Выделение цветом нужного пункта меню
-    var colorClicked = '#FFC107';
-    var color = 'yellow';    
+    var colorClicked = 'rgb(255, 193, 7)'; //оранжевый #FFC107
+    var colorDefault = 'rgb(255, 255, 153)'  // бледно-желтый #FFFF99
+    var colorHover = 'yellow'; 
+    var linkDesk = ".header ul li a";
+    var linkMob = '#header-mob li a';
 
     var changeColor = function (event) {
+        event.preventDefault();
         for (var i = 0; i < $('.header ul li a').length; i++) {
-            if ( $('.header ul li a').eq(i).text() !== $(event.target).text() ) {
-                $('.header ul li a').css('color', '#FFFF99');
-            } else $('.header ul li a').css('color', '#colorClicked');
+            if ( $(linkDesk).eq(i).text() !== $(event.target).text() ) {
+                $(linkDesk).css('color', colorDefault);                
+            } else $(linkDesk).css('color', '#colorClicked'); 
         }
     }
 
-    $(".header ul li a").click(changeColor);   
+    $(linkDesk).click(changeColor);   
 
 
     // Прокручивание до нужного раздела 
-    $('#header-mob li a, .header ul li a').click(function(event) {
-           
+    $(linkDesk + ',' + linkMob).click(function(event) {           
         $(this).css('color', colorClicked);
         var fixed_offset = 55;
         $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset}, 1,
@@ -25,6 +28,22 @@ $(function () {
                 $('.collapse').collapse('hide');
         });
         event.preventDefault();
+    });
+
+    // Подсветка цветом пункта меню при наведении мыши
+    $(linkDesk + ',' + linkMob).mouseover(function(event) {
+        for (var i = 0; i < $(linkDesk).length; i++) {
+            if ( $(linkDesk).eq(i).css('color') !== colorClicked ) { 
+                $(event.target).css('color', colorHover);
+            } 
+        }
+    });
+
+    // Изменение цвета пункта меню при убирании мыши 
+    $(linkDesk + ',' + linkMob).mouseleave(function(event) {
+        if ($(this).css('color') !== colorClicked ) {
+            $(this).css('color', colorDefault);
+        }      
     });
     
 
